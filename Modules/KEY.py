434286c56935ecbedd4f20c0d31fce7f6dd90aa0
Aziz <74332587+oh-az@users.noreply.github.com>
2022-11-13 09:36:36 -0500

    Delete Modules directory

--- Modules/KEY.py
+++
@@ -1,7 +0,0 @@
-from virus_total_apis import PublicApi as VirusTotalPublicApi
-
-keyFile = open("KEY.txt","r")
-
-API_KEY = keyFile.readline().strip("\n")
-
-virustotal = VirusTotalPublicApi(API_KEY)
